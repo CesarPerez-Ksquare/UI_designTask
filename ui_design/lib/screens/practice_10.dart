f81e13595f74d472416153be615c52ea6e9d7b30
Cesar Perez <cesar.perez@itksquare.edu.mx>
2022-10-25 00:49:29 -0400

    UI Design

--- ui_design/lib/screens/practice_10.dart
+++ ui_design/lib/screens/practice_10.dart
@@ -6,35 +6,50 @@ class Practice10 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        body: Container(
-          width: double.infinity,
-          height: double.infinity,
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Row(children: [
-                  Expanded(child: Image.asset('images/BgImage.png'))
+        backgroundColor: Colors.red,
+        body: Center(
+          child: Container(
+            height: 600,
+            width: 250,
+            color: Colors.white,
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                          child: Image.asset(
+                        'images/BgImage.png',
+                        height: 460,
+                        width: 250,
+                      )),
+                    ],
+                  ),
+                  OutlinedButton(
+                      style: ButtonStyle(
+                          shape: MaterialStateProperty.all<OutlinedBorder>(
+                              const RoundedRectangleBorder())),
+                      onPressed: () {},
+                      child: const Text(
+                        'Continue with Google',
+                        style: TextStyle(color: Colors.black),
+                      )),
+                  ElevatedButton(
+                      style: ButtonStyle(
+                          elevation: MaterialStateProperty.all(0),
+                          shape: MaterialStateProperty.all<OutlinedBorder>(
+                              const RoundedRectangleBorder())),
+                      onPressed: () {},
+                      child: const Text(
+                        'Continue with Facebook',
+                        style: TextStyle(color: Colors.white),
+                      )),
+                  const Text(
+                      "By signing up you're accepting our terms and conditions",
+                      style: TextStyle(fontSize: 12),
+                      textAlign: TextAlign.center)
                 ]),
-                OutlinedButton(
-                    style: OutlinedButton.styleFrom(
-                        side: const BorderSide(width: 1.0)),
-                    onPressed: () {},
-                    child: const Text(
-                      'Continue with Google',
-                      style: TextStyle(color: Colors.black),
-                    )),
-                ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        side: const BorderSide(width: 1.0)),
-                    onPressed: () {},
-                    child: const Text(
-                      'Continue with Facebook',
-                      style: TextStyle(color: Colors.white),
-                    )),
-                const Text(
-                    "By signing up you're accepting our terms and conditions")
-              ]),
+          ),
         ));
   }
 }
